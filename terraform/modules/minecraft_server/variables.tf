c1ad05ca721b5422e1efa58f1ceb87910ce00941
Stoaties <corentin.gouanvic@gmail.com>
2026-02-01 22:37:28 -0500

    Started new minecraft deployment workflow with terraform and jinja templating

---
+++ terraform/modules/minecraft_server/variables.tf
@@ -0,0 +1,15 @@
+variable "server_name"{
+  type = string
+}
+
+variable "server_memory"{
+  type = string
+}
+
+variable "server_version"{
+  type = string
+}
+
+variable "server_port"{
+  type = number
+}
