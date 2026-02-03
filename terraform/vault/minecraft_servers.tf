843851b4e13cbc988e548d024374a30bd159b08a
Stoaties <Stoaties@users.noreply.github.com>
2026-02-03 21:54:27 +0000

    Add greee2 on port 25566.

--- terraform/vault/minecraft_servers.tf
+++ terraform/vault/minecraft_servers.tf
@@ -5,3 +5,10 @@ module "greeeeeeeee" {
     server_port =  25565
     email =  corentin.gouanvic@gmail.com
   }
+module "greee2" {
+    source = "./modules/minecraft_server"
+    server_memory = 8G
+    server_version = "1.21"
+    server_port =  25566
+    email =  corentin.gouanvic@gmail.com
+  }
